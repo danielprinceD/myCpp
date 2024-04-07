35c47325013d42616a1921bffff43109c30fc49a
daniel <d.danielprince2003@gmail.com>
2024-04-07 17:57:56 +0530

    completed:LeetCode Problem 88

---
+++ LeetCode/88. Merge Sorted Array.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=0,j=0;
+        vector<int> temp;
+        while(i < m && j < n)
+        {
+            if(nums1[i] < nums2[j])
+            {
+                temp.push_back(nums1[i]);
+                i++;
+            }
+            else
+            {
+                temp.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while(i < m){temp.push_back(nums1[i]);i++;}
+        while(j < n){temp.push_back(nums2[j]);j++;}
+        nums1 = temp;
+    }
+};
+
+int main()
+{
+    std::cout<<"Hello World";
+
+    return 0;
+}
