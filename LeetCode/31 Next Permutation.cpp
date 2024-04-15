1f42609a46c3f6a113e7dfa47877ad07c7cbd066
daniel <d.danielprince2003@gmail.com>
2024-04-15 19:18:40 +0530

    completed:Problem 31

---
+++ LeetCode/31 Next Permutation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int maxi = *max_element(nums.begin(),nums.end());
+    }
+};
+
+int main()
+{
+}
