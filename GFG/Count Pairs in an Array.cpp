0db4f1579463036f49efdb091751d07d3351fa6b
daniel <d.danielprince2003@gmail.com>
2024-04-17 20:24:33 +0530

    completed:count Pairs in an Array

---
+++ GFG/Count Pairs in an Array.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+    public:
+    int merge(int arr[], int low, int mid, int high){
+        int temp[high-low+1];
+        int k=0;
+        int i=low;
+        int j=mid+1;
+        int inv=0;
+        
+        while(i<=mid && j<=high){
+            if(arr[i]<=arr[j]){
+                temp[k++]=arr[i++];
+            }
+            else{
+                inv+= mid-i +1;
+                temp[k++]=arr[j++];
+            }
+        }
+        
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        
+        while(j<=high){
+            temp[k++]=arr[j++];
+        }
+        
+        for(int x=low; x<=high; x++){
+            arr[x]=temp[x-low];
+        }
+        
+        return inv;
+    }
+    
+    int mergeSort(int arr[], int low, int high){
+        if(low>=high) return 0;
+        int inv=0;
+        
+        int mid= (low+high)/2;
+        
+        inv += mergeSort(arr, low, mid);
+        inv += mergeSort(arr, mid+1, high);
+        inv += merge(arr, low, mid, high);
+        
+        return inv;
+    }
+    
+    int inversionCount(int arr[],int N)
+    {
+        // Your Code Here
+        
+        int ans= mergeSort(arr, 0, N-1);
+        
+        return ans;
+        
+    }
+    int countPairs(int arr[] , int n ) 
+    {
+        int i=0 , j = 0, ans = 0;
+        for(int i=0;i<n;i++)arr[i]*=i;
+        return inversionCount(arr , n);
+    }
+};
+
+
+int main()
+{
+    int v[] = {8, 4, 2, 1};
+    Solution s;
+    cout<<s.countPairs(v , 4);
+    
+}
