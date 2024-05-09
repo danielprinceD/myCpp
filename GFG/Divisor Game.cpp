c99fa30d2c32be9bbd91d71cd4a6c84bb292823d
daniel <d.danielprince2003@gmail.com>
2024-05-09 15:49:28 +0530

    feat:Completed Divisor Game

---
+++ GFG/Divisor Game.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Solution {
+  public:
+    bool divisorGame(int n) {
+       return (n%2==0) ? true : false;
+    }
+};
+
+int main()
+{
+    std::cout<<"Hello World";
+
+    return 0;
+}
